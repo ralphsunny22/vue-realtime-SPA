264f8f66203c6cf437934372c7119567ffd389f7
ralphsunny22 <ralphsunny114@gmail.com>
2021-04-29 00:09:52 +0100

    Model Factory Resources Creation

---
+++ database/migrations/2021_04_28_225000_create_replies_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRepliesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('replies', function (Blueprint $table) {
+            $table->id();
+            $table->text('body');
+
+            $table->unsignedBigInteger('question_id');
+            $table->unsignedBigInteger('user_id');
+
+            //to delete all replies connected to a question, incase the question was deleted
+            $table->foreign('question_id')->references('id')->on('questions')->onDelete('cascade');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('replies');
+    }
+}
