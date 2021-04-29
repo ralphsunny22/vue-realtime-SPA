37496e2162e1392aa833cfba77f6ade40a8e1303
ralphsunny22 <ralphsunny114@gmail.com>
2021-04-29 12:34:07 +0100

    Like and Unlike Reply

--- app/Http/Controllers/LikeController.php
+++ app/Http/Controllers/LikeController.php
@@ -2,7 +2,8 @@
 
 namespace App\Http\Controllers;
 
-use App\Like;
+use App\Model\Like;
+use App\Model\Reply;
 use Illuminate\Http\Request;
 
 class LikeController extends Controller
@@ -12,9 +13,18 @@ class LikeController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function likeIt(Reply $reply)
     {
-        //
+        $reply->like()->create([
+            // 'user_id' => auth()->id()
+            'user_id' => "1"
+        ]);
+    }
+
+    public function unLikeIt(Reply $reply)
+    {
+        // $reply->like()->where('user_id', auth()->id())->first()->delete();
+        $reply->like()->where('user_id', '1')->first()->delete();
     }
 
     /**
